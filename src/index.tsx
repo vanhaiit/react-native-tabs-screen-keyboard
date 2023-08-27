057ca7776215c0a6979e7859389c671057bd5cbe
hai.le <hai.le@sotatek.com>
2023-08-27 22:14:54 +0700

    fix(hot fix context): hot fix code 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import * as React from 'react';
 import { useEffect, useRef, useState } from 'react';
 import {
   Keyboard,
